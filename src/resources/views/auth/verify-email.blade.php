57b29204474dff4da0b58eb1599e871b4b5a72c0
kaori <kaori.yamashita.jb@gmail.com>
2024-08-12 14:20:09 +0900

    メール認証確認画面デザイン修正

--- src/resources/views/auth/verify-email.blade.php
+++ src/resources/views/auth/verify-email.blade.php
@@ -1,29 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="verify-email__content">
+    <div class="verify-email__card">
+        <h2 class="verify-email__header">{{ __('メールアドレス受信確認用のメールを送信しました') }}</h2>
 
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('メールアドレスの確認') }}</div>
+        <div class="verify-email__body">
+            <p>{{ auth()->user()->email }} へ受信確認用のメールを送信しました。</p>
+            <p>メールをご確認いただき、メールに記載された URL をクリックして、Atteへの登録を完了してください。</p>
 
-                    <div class="card-body">
-                        @if (session('resent'))
-                            <div class="alert alert-success" role="alert">
-                                {{ __('新しい確認リンクがメールアドレスに送信されました。') }}
-                            </div>
-                        @endif
-
-                        {{ __('続行する前に、メールで確認リンクをご確認ください。') }}
-                        {{ __('メールが届いていない場合') }},
-                        <form class="d-inline" method="POST" action="{{ route('verification.send') }}">
-                            @csrf
-                            <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('ここをクリックして再送信してください') }}</button>.
-                        </form>
-                    </div>
+            @if (session('resent'))
+                <div class="verify-email__alert">
+                    {{ __('新しい確認リンクがメールアドレスに送信されました。') }}
                 </div>
-            </div>
+            @endif
+
+            <h3 class="verify-email__subheader">{{ __('メールが届かない場合') }}</h3>
+            <p>受信確認メールが届かない場合、以下をご確認ください。</p>
+            <ul class="verify-email__list">
+                <li>迷惑メールフォルダに振り分けられていたり、フィルターや転送の設定によって受信ボックス以外の場所に保管されていないかご確認ください</li>
+                <li>メールの配信に時間がかかる場合がございます。数分程度待った上で、メールが届いているか再度ご確認ください</li>
+                <li>登録にご使用のメールアドレス {{ auth()->user()->email }} が正しいかどうか確認してください。正しくない場合は、メールアドレスを再設定してください</li>
+            </ul>
+
+            <form class="verify-email__form" method="POST" action="{{ route('verification.send') }}">
+                @csrf
+                <button type="submit" class="verify-email__button">{{ __('確認メールを再送信') }}</button>
+            </form>
         </div>
     </div>
+</div>
+@endsection
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/verify-email.css') }}">
 @endsection
